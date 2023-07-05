7df3ad689f41684794fc0608db61e16dd1713d6b
Ivan Chromjak <ivan@chromjak.com>
2023-07-05 18:00:54 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,9 @@ module.exports = {
     },
   },
   plugins: [
-    // ...
-    // require('@tailwindcss/forms'),
-    // require('@tailwindcss/aspect-ratio'),
-    // require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
   ]
 };
   
